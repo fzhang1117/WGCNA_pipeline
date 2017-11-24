0d8665d815629fa7ef8771109f847c8680595f89
zf <fzhang1117@mpimp-golm.mpg.de>
2017-11-24 17:28:58 +0100

    zf

--- WGCNA_dissTOM.R
+++ WGCNA_dissTOM.R
@@ -18,7 +18,7 @@ my.dissTOM <- function(expression, filebase = "myWGCNA", method.cor = "pearson",
     
     # This is the softhreshold select codes
     expression <- t(expression)
-    powers <- c(c(1: 10), seq(from = 12, to = 20, by = 2))
+    powers <- c(c(1: 10), seq(from = 12, to = 100, by = 2))
     sft <- pickSoftThreshold(expression, powerVector = powers, verbose = verbose)
     pdf(paste("./result/",  filebase, ".pdf", sep = ""))
         mat <- matrix(c(1, 2), 1, 2)
@@ -35,9 +35,9 @@ my.dissTOM <- function(expression, filebase = "myWGCNA", method.cor = "pearson",
     if(method.cor == 'pearson'){
         adjacency <- adjacency(datExpr = expression, type = type.net, power = softPower, corFnc = "cor", corOptions = "use = 'p'")
     }else if(method.cor == 'spearman'){
-        adjacency <- adjacency(datExpr = expression, type = type.net, power = softPower, conFnc = "cor", corOptions = "method = 'spearman")
+        adjacency <- adjacency(datExpr = expression, type = type.net, power = softPower, corFnc = "cor", corOptions = "use = 'p', method = 'spearman'")
     }else if(method.cor == 'bicor'){
-        adjacency <- adjacency(datExpr = expression, type = type.net, power = softPower, conFnc = "bicor")
+        adjacency <- adjacency(datExpr = expression, type = type.net, power = softPower, corFnc = "bicor")
     }
     
     
@@ -49,3 +49,23 @@ my.dissTOM <- function(expression, filebase = "myWGCNA", method.cor = "pearson",
     return(dissTOM)    
 }
 
+#dissTOM.JC <- my.dissTOM(ave.JC, filebase = "JC.pcc", method.cor = "pearson", type.net = 'unsigned', verbose = 5)
+#dissTOM.JS <- my.dissTOM(ave.JS, filebase = "JS.pcc", method.cor = "pearson", type.net = 'unsigned', verbose = 5)
+#dissTOM.HC <- my.dissTOM(ave.HC, filebase = "HC.pcc", method.cor = "pearson", type.net = 'unsigned', verbose = 5)
+#dissTOM.HS <- my.dissTOM(ave.HS, filebase = "HS.pcc", method.cor = "pearson", type.net = 'unsigned', verbose = 5)
+
+#save('dissTOM.JC', 'dissTOM.JS', 'dissTOM.HC', 'dissTOM.HS', list = c('dissTOM.JC', 'dissTOM.JS', 'dissTOM.HC', 'dissTOM.HS'), file = "./data/dissTOM.pearson.Rdata")
+
+dissTOM2.JC <- my.dissTOM(ave.JC, filebase = "JC.sp", method.cor = "spearman", type.net = 'unsigned', verbose = 5)
+dissTOM2.JS <- my.dissTOM(ave.JS, filebase = "JS.sp", method.cor = "spearman", type.net = 'unsigned', verbose = 5)
+dissTOM2.HC <- my.dissTOM(ave.HC, filebase = "HC.sp", method.cor = "spearman", type.net = 'unsigned', verbose = 5)
+dissTOM2.HS <- my.dissTOM(ave.HS, filebase = "HS.sp", method.cor = "spearman", type.net = 'unsigned', verbose = 5)
+
+save('dissTOM2.JC', 'dissTOM2.JS', 'dissTOM2.HC', 'dissTOM2.HS', list = c('dissTOM.JC', 'dissTOM.JS', 'dissTOM.HC', 'dissTOM.HS'), file = "./data/dissTOM.spearman.Rdata")
+
+dissTOM3.JC <- my.dissTOM(ave.JC, filebase = "JC.bicor", method.cor = "bicor", type.net = 'unsigned', verbose = 5)
+dissTOM3.JS <- my.dissTOM(ave.JS, filebase = "JS.bicor", method.cor = "bicor", type.net = 'unsigned', verbose = 5)
+dissTOM3.HC <- my.dissTOM(ave.HC, filebase = "HC.bicor", method.cor = "bicor", type.net = 'unsigned', verbose = 5)
+dissTOM3.HS <- my.dissTOM(ave.HS, filebase = "HS.bicor", method.cor = "bicor", type.net = 'unsigned', verbose = 5)
+
+save('dissTOM3.JC', 'dissTOM3.JS', 'dissTOM3.HC', 'dissTOM3.HS', list = c('dissTOM.JC', 'dissTOM.JS', 'dissTOM.HC', 'dissTOM.HS'), file = "./data/dissTOM.bicor.Rdata")
